75421c2caa2b5d8d39539e97b8649d271ef70de8
Mattias Hemmingsson <matte@elino.se>
2019-02-04 20:31:46 +0100

    Update

--- sendToFirebase.py
+++ sendToFirebase.py
@@ -1,7 +1,8 @@
-from firebase import firebase
 import json
 import os
 import datetime
+import time
+from mailchimp3 import MailChimp
 
 
 
@@ -9,53 +10,139 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 import json
+import os
 
 # Fetch the service account key JSON file contents
-cred = credentials.Certificate('/code/fins-dff79-firebase-adminsdk-lozi2-715a0adc3f.json')
-USER_ID='7qwtyt4vTqN5OWdeTaLWsfcfALH2'
-MATCH='wordpress'
-MAILCHIMP_LIST="5555"
-
+cred = credentials.Certificate(os.environ['FIREBASE_AUTH'])
+USER_ID=os.environ['USER_ID']
+MATCH=os.environ['MATCH']
+MAILCHIMP_LIST=os.environ['MAILCHIMP_LIST']
+MAILCHIMP_API=os.environ['MAILCHIMP_API']
+HOW_MANY=int(os.environ['HOW_MANY'])
 # Initialize the app with a service account, granting admin privileges
 firebase_admin.initialize_app(cred, {
-    'databaseURL': 'https://fins-dff79.firebaseio.com'
+    'databaseURL': os.environ['FIREBASE']
 })
 
+client = MailChimp(mc_api=MAILCHIMP_API)
 
 
-ref = db.reference('todos/'+USER_ID+'/emails')
-ref_user = db.reference('todos/'+USER_ID+'/emails')
-db_values = ref_user.get()
-for values in db_values:
-	for values2 in db_values[values]:
-		#print(values2)
-		if values2 == "words":
-			if MATCH in db_values[values][values2]:
-				'''
-				Great we have a email and that matched the words we are using lets add it to the mailchimp list
-				'''
-				print(db_values[values]['EmailCampan'])
+userToDelete=[]
+
+def deleteFromMailchimp(userhash):
+	'''
+	Delet the user from the list
+	'''
+	client.lists.members.delete(list_id=MAILCHIMP_LIST, subscriber_hash=userhash)
 
 
-				#Lest se if we already have sent a email to this person
-				if MAILCHIMP_LIST in db_values[values]['EmailCampan']:
-					print("We habe already used this email here")
 
+def addToMailchimp(email):
+	'''
+	Add the email to our mailchimp list
+	'''
+	
+	try:
+		backFromMailchimp=client.lists.members.create(MAILCHIMP_LIST, {
+		    'email_address': '{0}'.format(email),
+		    'status': 'subscribed',
+		    'merge_fields': {
+		        'FNAME': '{0}'.format("Automated "),
+		        'LNAME': '{0}'.format("Found"),
+		           },
+		})
+		userToDelete.append(backFromMailchimp['id'])
+
+	except:
+		print("Error update user")
+
+
+def findAndAddEmail():
+	'''
+	Here is where we find and add emails
+	'''
+
+	ref = db.reference('todos/'+USER_ID+'/emails')
+	ref_user = db.reference('todos/'+USER_ID+'/emails')
+	db_values = ref_user.get()
+	haveDone=0
+	for values in db_values:
+		for values2 in db_values[values]:
+			if values2 == "words":
+				if MATCH in db_values[values][values2]:
+					'''
+					Great we have a email and that matched the words we are using lets add it to the mailchimp list
+					'''
+				
+	
+					#Lest se if we already have sent a email to this person
+					if MAILCHIMP_LIST in db_values[values]['EmailCampan']:
+						print("We habe already used this email here")
+	
+					else:
+						'''
+						Nice lets add the email to the list and update profile
+						'''
+						if HOW_MANY >= haveDone:
+							user = db_values[values]
+							box_ref = ref.child(values)
+							list_list=db_values[values]['EmailCampan']
+							list_list.append(MAILCHIMP_LIST)
+							#Add the user to Mailchimp
+							addToMailchimp(db_values[values]['email'])
+	
+							updated =box_ref.update({
+	    								'EmailCampan': list_list
+									})
+	
+							#Update how many we have done 
+							haveDone=haveDone + 1
+							print(updated)
+						else:
+							print("We have already as many as we need")
+	
+					print(db_values[values])
 				else:
-					'''
-					Nice lets add the email to the list and update profile
-					'''
-					user = db_values[values]
-					box_ref = ref.child(values)
-					list_list=db_values[values]['EmailCampan']
-					list_list.append(MAILCHIMP_LIST)
-					updated =box_ref.update({
-    							'EmailCampan': list_list
-							}) 
-
-					print(updated)
+					print("No match in words for email")
 
 
-				print(db_values[values])
 
 
+	#########################################
+	##
+	##  Time to clean up and delet the users from the mailchimp list
+	
+	time.sleep(300)
+	for user_hash in userToDelete:
+		deleteFromMailchimp(user_hash)
+
+
+########################################
+##
+## Function to reset all users list
+
+def clear_emaild_users():
+	'''
+	Loop and clear out what user has bean to what list
+
+
+	'''
+	ref = db.reference('todos/'+USER_ID+'/emails')
+	ref_user = db.reference('todos/'+USER_ID+'/emails')
+	db_values = ref_user.get()
+	
+	for values in db_values:
+		for values2 in db_values[values]:
+				user = db_values[values]
+				print(user)
+				box_ref = ref.child(values)
+				#Add the user to Mailchimp
+				
+				updated =box_ref.update({
+    						'EmailCampan': ['xxx']
+						})
+
+						#Update how many we have done 
+				print(updated)
+findAndAddEmail()				
+#clear_emaild_users()
